c8dbc846f8162b5037a0e34ee8e2ad30b627793c
rizalraisyad <muharamrizal2@gmail.com>
2021-04-07 01:59:10 +0700

    last commit

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+
 const router = express.Router();
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 router.param('id', tourController.checkID);
 // crete a checkboddy middleware
